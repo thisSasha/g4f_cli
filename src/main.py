87ccd42e1959f47b0efd39ac4411755eab549609
thisSasha <thisDevSasha@gmail.com>
2025-06-17 21:32:21 +0300

    Update (main.py): Add CommandFactory classes

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ from g4f_execute import (
     g4f_setAllModels,
 )
 from cli import setup_cli, cli_prompt
+from abc import ABC, abstractmethod
 
 
 g4f = None
@@ -17,17 +18,7 @@ logger = None
 # Setup varaibles
 #
 
-STYLE_COMMANDS = [
-    "help",
-    "exit",
-    "find",
-    "copy",
-    "use",
-    "echo",
-    "get",
-    "update",
-    "version",
-]
+STYLE_COMMANDS = []
 DEBUG = False
 FILES = {
     "logo": "public/logo.txt",
@@ -56,7 +47,7 @@ general_toCopy = []
 
 def setup_modules():
     global logger
-    logger = setup_cli(PROMPT_STYLE, FLAG_MAP, STYLE_COMMANDS)
+    logger = setup_cli(PROMPT_STYLE, FLAG_MAP, STYLE_COMMANDS, DEBUG)
     setup_g4f(g4f, logger, print_toCopy, DEBUG)
 
 
@@ -69,7 +60,7 @@ def print_toCopy(arr):
         return
     global general_toCopy
     for i, val in enumerate(arr):
-        print(f"[{i}]: {val}")
+        print(f"[\033[93m{i}\033[0m]: {val}\033[0m")
     general_toCopy = []
     general_toCopy = arr
     logger.info('To copy some, type "copy <number>"')
@@ -83,11 +74,168 @@ def ensure_g4f():
         g4f = _g4f
 
 
-def command_help(type):
-    if type == "html":
-        webbrowser.open("https://thisSasha.github.io/g4f_cli")
-    elif type == "py":
-        print(open(FILES["help"], "r").read())
+#
+# CommandsFactory
+#
+
+
+class Command(ABC):
+    @abstractmethod
+    def execute(self, args, flags, command):
+        pass
+
+
+class CommandsFactory:
+    def __init__(self):
+        self.commands = {}
+
+    def get(self, name):
+        return self.commands.get(name)
+
+    # Decorator
+    def register(self, name):
+        def decorator(func):
+            self.commands[name] = func
+            return func
+
+        return decorator
+
+
+commandFactory = CommandsFactory()
+
+
+@commandFactory.register("help")
+class CommandHelp:
+    name = "help"
+
+    def execute(args, flags):
+        type = flags.get("type", "py")
+        if type == "html":
+            webbrowser.open("https://thisSasha.github.io/g4f_cli")
+        elif type == "py":
+            print(open(FILES["help"], "r").read())
+
+
+@commandFactory.register("exit")
+class CommandExit:
+    name = "exit"
+
+    def execute(args, flags):
+        sys.exit(0)
+
+
+@commandFactory.register("find")
+class CommandFind:
+    name = "find"
+
+    def execute(args, flags):
+        typeToFind = "provider" if flags.get("provider") else "model"
+        keysRule = "or" if flags.get("or") else "and"
+        g4f_find(typeToFind, keysRule, args[1:])
+
+
+@commandFactory.register("copy")
+class CommandCopy:
+    name = "copy"
+
+    def execute(args, flags):
+        if len(args) < 2 or not args[1].isdigit():
+            logger.error("Invalid command format. Type 'help' for usage.")
+            return
+
+        index = int(args[1])
+        if 0 <= index < len(general_toCopy):
+            logger.info("Copied: " + general_toCopy[index])
+            pyperclip.copy(general_toCopy[index])
+        else:
+            logger.error("Invalid index")
+
+
+@commandFactory.register("use")
+class CommandUse:
+    name = "use"
+
+    def execute(args, flags):
+        if len(args) < 2:
+            logger.error("Invalid command format. Type 'help' for usage.")
+            return
+
+        useModel = flags.get("model", "default")
+        useProvider = flags.get("provider", "BaseProvider")
+        useText = args[1]
+        g4f_call(useModel, useProvider, useText)
+
+
+@commandFactory.register("echo")
+class CommandEcho:
+    name = "echo"
+
+    def execute(args, flags):
+        logger.info(" ".join(args[1:]))
+
+
+@commandFactory.register("get")
+class CommandGet:
+    name = "get"
+
+    def execute(args, flags):
+        entity_type = (
+            "providers"
+            if flags.get("providers")
+            else "models" if flags.get("models") else False
+        )
+        if flags.get("all", False):
+            if entity_type:
+                g4f_getAll(entity_type)
+            else:
+                print_toCopy([general_allModels])
+        elif entity_type and len(args) > 1:
+            g4f_get(entity_type, args[1])
+        else:
+            logger.error("Invalid command format. Type 'help' for usage.")
+
+
+@commandFactory.register("update")
+class CommandUpdate:
+    name = "update"
+
+    def execute(args, flags):
+        if flags.get("only-cli", False):
+            logger.info("Updating g4f_cli...")
+            process = subprocess.Popen(
+                [sys.executable, "-m", "pip", "install", "-U", "."],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                cwd=os.path.dirname(os.path.dirname(__file__)),
+            )
+            stdout, stderr = process.communicate()
+            if process.returncode == 0:
+                logger.info("g4f_cli updated successfully. Please restart the CLI.")
+            else:
+                logger.error(f"Failed to update g4f_cli: {stderr.decode()}")
+            return
+
+        logger.info("Trying to update g4f...")
+        process = subprocess.Popen(
+            [sys.executable, "-m", "pip", "install", "-U", "g4f"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        stdout, stderr = process.communicate()
+        if "installing packages" in stdout.decode():
+            logger.info("g4f updated successfully")
+        else:
+            logger.info("The latest version of g4f is installed")
+
+
+@commandFactory.register("version")
+class CommandVersion:
+    name = "version"
+
+    def execute(args, flags):
+        logger.info(f"G4F_CLI v{VERSION}")
+        logger.info("Author: thisDevSasha")
+        logger.info("Source code: https://github.com/thisSasha/g4f_cli")
 
 
 #
@@ -99,9 +247,10 @@ print(open(FILES["logo"], "r").read())
 if DEBUG:  # print
     print("\033[93mLoading g4f...\033[0m")
 ensure_g4f()
-setup_modules()
 if DEBUG:  # print
     print("\033[93mSuccessfully loaded\033[0m")
+STYLE_COMMANDS = commandFactory.commands.keys()
+setup_modules()
 general_allModels = g4f_setAllModels()
 interrupt_counter = 0
 
@@ -111,65 +260,9 @@ while True:
         interrupt_counter = 0
         if command.strip() == "":
             continue
-        elif command == "help":
-            command_help(flags.get("type", "py"))
-        elif command == "echo":
-            logger.info(" ".join(args[1:]))
-        elif command == "find":
-            typeToFind = "provider" if flags.get("provider") else "model"
-            keysRule = "or" if flags.get("or") else "and"
-            g4f_find(typeToFind, keysRule, args[1:])
-        elif command == "copy":
-            if args[1].isdigit():
-                index = int(args[1])
-                if 0 <= index < len(general_toCopy):
-                    logger.info("Copied: " + general_toCopy[index])
-                    pyperclip.copy(general_toCopy[index])
-                else:
-                    logger.error("Invalid index")
-            else:
-                logger.error("Index must be a number")
-        elif command == "use":
-            useModel = flags.get("model", "default")
-            useProvider = flags.get("provider", "BaseProvider")
-            useText = args[1]
-            g4f_call(useModel, useProvider, useText)
-        elif command == "exit":
-            break
-        elif command == "version":
-            logger.info(f"G4F_CLI v{VERSION}")
-            logger.info("Author: thisDevSasha")
-            logger.info("Source code: https://github.com/thisSasha/g4f_cli")
-        elif command == "get":
-            entity_type = (
-                "providers"
-                if flags.get("providers")
-                else "models" if flags.get("models") else False
-            )
-            if flags.get("all", False):
-                if entity_type:
-                    g4f_getAll(entity_type)
-                else:
-                    print_toCopy([general_allModels])
-            elif entity_type:
-                g4f_get(entity_type, args[1])
-            else:
-                logger.error("Invalid command format. Type 'help' for usage.")
-        elif command == "update":
-            if flags.get("--only-cli", False):
-
-                continue
-            logger.info("Trying to update g4f...")
-            process = subprocess.Popen(
-                [sys.executable, "-m", "pip", "install", "-U", "g4f"],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            stdout, stderr = process.communicate()
-            if "installing packages" in stdout.decode():
-                logger.info("g4f updated successfully")
-            else:
-                logger.info("The latest version of g4f is installed")
+        cmd = commandFactory.get(command)
+        if cmd:
+            cmd.execute(args, flags)
         else:
             logger.error('Command not found. Type "help" for a list of commands.')
     except KeyboardInterrupt:
