d57385c1c0fbf9721581446c8e5441cb7dc75286
thisSasha <thisDevSasha@gmail.com>
2025-06-17 21:45:45 +0300

    Update (cli.py): Add autocomplete

--- src/cli.py
+++ src/cli.py
@@ -1,32 +1,48 @@
 import logging, re
 from rich.logging import RichHandler
-from prompt_toolkit import prompt
+from prompt_toolkit import prompt, PromptSession
 from prompt_toolkit.lexers import Lexer
 from prompt_toolkit.styles import Style
+from prompt_toolkit.completion import Completer, Completion
 from prompt_toolkit.history import InMemoryHistory
 
 PROMPT_STYLE = None
 FLAG_MAP = None
 STYLE_COMMANDS = None
+DEBUG = None
 args = []
 flags = {}
+session = None
 
-def setup_cli(PROMPT_STYLE_, FLAG_MAP_, STYLE_COMMANDS_):
+
+def setup_cli(PROMPT_STYLE_, FLAG_MAP_, STYLE_COMMANDS_, DEBUG_):
     setup_logger()
-    global FLAG_MAP, STYLE_COMMANDS, PROMPT_STYLE
+    global FLAG_MAP, STYLE_COMMANDS, PROMPT_STYLE, DEBUG, session
     PROMPT_STYLE = Style.from_dict(PROMPT_STYLE_)
     FLAG_MAP = FLAG_MAP_
     STYLE_COMMANDS = STYLE_COMMANDS_
+    DEBUG = DEBUG_
+    session = PromptSession(
+        completer=CommandCompleter(STYLE_COMMANDS),
+        complete_while_typing=True,
+        history=prompt_history,
+        lexer=CommandLexer(),
+        style=PROMPT_STYLE,
+    )
     return logger
-    
 
 
 def setup_logger():
-    global logger
+    global logger, DEBUG
 
     logger = logging.getLogger("g4f_cli")
     logger.setLevel(logging.DEBUG)
-    rich_handler = RichHandler(markup=True, show_time=False, show_path=False)
+    rich_handler = RichHandler(
+        markup=True,
+        show_time=False,
+        show_path=False,
+        level=logging.DEBUG if DEBUG else logging.INFO,
+    )
     rich_handler.setFormatter(logging.Formatter("%(message)s"))
 
     logger.handlers = [rich_handler]
@@ -70,6 +86,17 @@ class CommandLexer(Lexer):
         return get_tokens
 
 
+class CommandCompleter(Completer):
+    def __init__(self, commands):
+        self.commands = commands
+
+    def get_completions(self, document, complete_event):
+        word = document.text
+        for cmd in self.commands:
+            if cmd.startswith(word):
+                yield Completion(cmd, start_position=-len(word))
+
+
 def prompt_processArg(arg, cmd):
     if arg.startswith("--"):
         if "=" in arg:
@@ -91,7 +118,6 @@ def prompt_processArg(arg, cmd):
         args.append(arg)
 
 
-
 def prompt_processRequest(request):
     quotes = False
     current_arg = ""
@@ -110,13 +136,13 @@ def prompt_processRequest(request):
     if current_arg:
         prompt_processArg(current_arg, cmd)
 
+
 prompt_history = InMemoryHistory()
+
 
 def cli_prompt(promt_value):
     global args, flags
-    request = prompt(
-        promt_value, lexer=CommandLexer(), style=PROMPT_STYLE, history=prompt_history
-    )
+    request = session.prompt(promt_value)
     args = []
     flags = {}
     prompt_processRequest(request=request)
